f68147bb46590e0e6b66fefcbf70c728ee6be8bd
Gabriel Sahid Gomez <gabrielsahidgomez@gmail.com>
2022-03-28 23:47:45 -0300

    Añadiendo menu ecommerse

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+const NavBar = () => {
+    return(
+        <nav>
+            <a href="#">Tienda</a>
+            <a href="#">Categorias</a>
+            <a href="#">Noticias</a>
+            <a href="#">Destacados</a>
+            <a href="#">Contacto</a>
+            <div class="animation start-home"></div>
+        </nav>
+    )
+}
+
+export default NavBar
