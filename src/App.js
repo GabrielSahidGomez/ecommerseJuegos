dfbcc8b7ecd12c9b7f0bf3d2525c691c6e2834a1
Gabriel Sahid Gomez <gabrielsahidgomez@gmail.com>
2022-04-08 00:54:23 -0300

    contador

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import ItemListConstainer from "./components/ItemListContainer/ItemListcontainer";
+import ItemCount from "./components/ItemCount/ItemCount.js"
+
 function App() {
+
+  const handleOnAdd = (quantity) => {
+    console.log(`se agregaron ${quantity} juegos`)
+  }
 
   return (
 
@@ -15,7 +21,8 @@ function App() {
 
         <NavBar />
         
-
+        <ItemCount initial={0} stock={10} onAdd={handleOnAdd}/>
+        
 
     </>
 
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,32 @@
+import "./ItemCount.css"
+import {useState} from "react"
+
+const Counter = ({initial, stock, onAdd}) => {
+    const [count, setCount] = useState(initial)
+
+    const decrement = () => {
+        if(count > initial)
+        setCount(count - 1)
+        }
+    
+    const increment = () => {
+        if(count < stock)
+            setCount(count + 1)
+        }
+
+        return(
+
+            <div className="container">
+                <div className="container-button">
+                    <button className="button" onClick={increment}> + </button>
+                        <p> {count} </p>
+                    <button className="button dec" onClick={decrement}> - </button>    
+                </div>
+                <div className="a">
+                <button onClick={ () => onAdd(count)}> Agregar al carrito </button>
+                </div>
+            </div>
+        )    
+    }
+
+export default Counter;
